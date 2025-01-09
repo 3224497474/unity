a1e12a8d36c570a292c2e0d18b3ee239b9141a63
HolyUncleLi <46733173+HolyUncleLi@users.noreply.github.com>
2025-01-09 23:28:51 +0800

    早期更新
    1.终端不弹出黑框
    2.添加角色对话动画
    3.添加一个非完善角色配置表

---
+++ Assets/GameAssets/RogueLevel/Scripts/Player/PlayerControllor_rogue.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllor_rogue : MonoBehaviour
+{
+    public static PlayerControllor_rogue instance;
+
+    private void Awake()
+    {
+        instance = this;        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
